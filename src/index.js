5df58d7a9f699a5dc3e5b802750b82fde19f1e78
yeji <lyj.lee@apptest.ai>
2021-10-07 16:26:12 +0900

    :hammer: 다음 챕터 학습을 위해 사용하지 않을 에셋, 파일, 의존성 삭제

--- src/index.js
+++ src/index.js
@@ -1,38 +1,13 @@
 import _ from 'lodash'; //array, collection, date 등 데이터의 필수적인 구조를 쉽게 다룰 수 있게 하는 데 사용된다.
-import './style.css';
-import Logo from './logo.png';
-import Data from './data.xml';
-import Notes from './data.csv';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
+
 //json 파일을 import 할 때는 위와 같은 디폴트 import 만 경고없이 사용할 수 있다.
 //ex) import { foo } from './data.json' 같은 형식은 스펙에서 허용하지 않으므로 경고가 노출된다.
-
-console.log(toml.title); //output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
     const element = document.createElement('div');
 
     // lodash 사용
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    // 원래 있던 div 에 이미지를 추가합니다.
-    const myLogo = new Image();
-    myLogo.src = Logo;
-
-    element.appendChild(myLogo);
-
-    console.log(Data);
-    console.log(Notes);
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,4 @@
 const path = require('path');
-const toml = require('toml');
-const yaml = require('yamljs');
-const json5 = require('json5');
 
 module.exports = {
     entry: './src/index.js',
@@ -10,54 +7,4 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
     },
     mode: 'development',
-    module: {
-        rules: [
-            {//모듈이 실행될 때 html 파일의 <head>에 문자열화 된 css가 <style>태그로 삽입됩니다. 
-                //dist 폴더의 index.html 을 실행시키고 브라우저에서 페이지 검사를 하면 head태그 안에
-                //style 태그가 추가된 걸 볼 수 있습니다.
-                test: /\.css$/i,
-                //아래 로더 순서 유지 되어야 한다.
-                use: ['style-loader', 'css-loader'],
-            },
-            {//웹팩5부터는 내장된 asset modules를 사용하여 이미지도 시스템에 쉽게 통합할 수 있다.
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {//폰트 파일 처리
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-            //csv, tsv, xml 같은 데이터들도 로드할 수 있다.
-            {
-                test: /\.(csv|tsv)$/i,
-                use: ['csv-loader'],
-            },
-            {
-                test: /\.xml$/i,
-                use: ['xml-loader'],
-            },
-            //loader가 아닌 custom parser를 이용하여 toml, yaml, json5 파일을 JSON모듈로 가져옴
-            {
-                test: /\.toml$/i,
-                type: 'json',
-                parser: {
-                    parse: toml.parse,
-                },
-            },
-            {
-                test: /\.yaml$/i,
-                type: 'json',
-                parser: {
-                    parse: yaml.parse,
-                },
-            },
-            {
-                test: /\.json5$/i,
-                type: 'json',
-                parser: {
-                    parse: json5.parse,
-                }
-            }
-        ]
-    }
 };

728370e6b58a4f8f9b32c4a6e12048657e0b2384
yeji <lyj.lee@apptest.ai>
2021-10-07 19:50:52 +0900

    :hammer: async-await 함수를 이용한 import() 동적 코드 스플리팅

--- src/index.js
+++ src/index.js
@@ -3,23 +3,11 @@
 
 function getComponent() {
     const element = document.createElement('div');
+    //import() 는 promise를 반환하므로 async-await 함수와 함께 사용할 수 있다.
+    const { default: _ } = await import('lodash');
 
-    //lodash를 동적으로 가져와 청크를 분리
-    return import('lodash')
-    .then(({ default: _ }) => {
-        const element = document.createElement('div');
-
-        element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-        return element;
-    })
-    .catch((error) => 'An error occurred while loading the component');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 }
-/*
-default가 필요한 이유는 webpack 4 이후로 CommonJS 모듈을 가져올 때 더 이상 
-module.exports 값 으로 해석되지 않으며 대신 CommonJS 모듈에 대한 인공 네임 스페이스 객체를 생성하기 때문입니다. 
-무슨 뜻이냐하면 module.exports 를 {default: 값} 이렇게 객체와 사용해야지 객체 형태가 아닌 것과 함께 사용하면 문제가 발생한다는 뜻.
-*/
 
 getComponenet().then((component) => {
     document.body.appendChild(component);

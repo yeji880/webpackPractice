da2c389ac9b219598d23936bdbbbe2dbd5b03313
yeji <lyj.lee@apptest.ai>
2021-10-07 16:49:02 +0900

    :hammer: html-webpack-plugin을 이용

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     //entry 지점 두곳으로 설정
@@ -6,6 +7,13 @@ module.exports = {
         index: './src/index.js',
         print: './src/print.js',
     },
+    plugins: [
+        //dist 폴더에 이미 index.html 파일이 있어도 html-webpack-plugin은 새로
+        //index.html 파일을 생성하고 기존 파일은 이 새로 생성된 파일로 대체된다.
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+        }),
+    ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
